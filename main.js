7b2a6c3b752bdc989b058d6eea66b8561b1e6865
Gabriel Soares Costa <gabrielsoarescosta.gsc@gmail.com>
2021-09-20 22:15:27 -0300

    Finished styles and javascript (with toggle rotation, active option)

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 /* ########## SHOW MENU ########## */
+const showMenu = (navbarId, toggleId, contentId) => {
+  const navbar = document.getElementById(navbarId);
+  const toggle = document.getElementById(toggleId);
+  const content = document.getElementById(contentId);
 
-/* ########## LINK ACTIVE COLOR ########## */
+  if (toggle && navbar) {
+    toggle.addEventListener("click", () => {
+      navbar.classList.toggle("show");
+      toggle.classList.toggle("rotate");
+      content.classList.toggle("expander");
+    });
+  }
+};
+
+showMenu("sidebar", "nav-toggle", "content");
+
+/* ########## MENU ACTIVE COLOR ########## */
+const menuOption = document.querySelectorAll(".nav__link");
+
+function activeColor() {
+  menuOption.forEach((op) => op.classList.remove("active"));
+  this.classList.add("active");
+}
+
+menuOption.forEach((o) => o.addEventListener("click", activeColor));
